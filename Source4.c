7452eced197eb27262f73809cb97db80d43667b7
Mihael72 <mihael.baresic@gmail.com>
2020-11-08 14:41:53 +0100

    Sum of polinoms task done

--- Source4.c
+++ Source4.c
@@ -4,7 +4,6 @@
 #include <string.h>
 
 
-
 struct _polinom;
 typedef struct _polinom* Position;
 typedef struct _polinom {
@@ -15,8 +14,10 @@ typedef struct _polinom {
 
 
 Position createPolinom(int, int);
-void insertStart(Position, Position);
+void insertEnd(Position, Position);
 void printList(Position);
+void sumPolinom(Position, Position, Position);
+void readFile(char* fileName,Position,Position);
 
 int main() {
 	FILE* File;
@@ -24,42 +25,26 @@ int main() {
 	Position Q = NULL;
 	polinom head1;
 	polinom head2;
+	polinom head3;
 	head1.next = NULL;
 	head2.next = NULL;
+	head3.next = NULL;
 	int coeff = 0, exp = 0;
-	char datName[256];
+	char fileName[256];
 	printf("Insert file name: ");
-	scanf("%s",datName);
-	File = fopen(datName, "r");
-	if (!File)
-		printf("File failed to open!");
+	scanf("%s",fileName);
+	
+	readFile(fileName,&head1,&head2);
 
-	int n=0,i;
-	char* buffer = malloc(1024);
-	while (1) {
-		if (fgets(buffer, 50, File) == '\0')
-			break;
-		n++;
-	}
-	rewind(File);
-	char c;
-	while (!feof(File)) 
-	{
-		c = fgetc(File);
-		if (c != '\n') {
-			fscanf(File, "%d %d", &coeff, &exp);
-			P = createPolinom(coeff, exp);
-			insertStart(&head1, P);
-		}else {
-			while (!feof(File)) {
-				fscanf(File, "%d %d", &coeff, &exp);
-				Q = createPolinom(coeff, exp);
-				insertStart(&head2, Q);
-			}
-		}
-	}
+	printf("\nList1 content:\n\tcoeff\texp\n");
 	printList(&head1);
+	printf("\nList2 content:\n\tcoeff\texp\n");
 	printList(&head2);
+
+	sumPolinom(&head1, &head2, &head3);
+	printf("\nSum of polinoms list content:\n\tcoeff\texp\n");
+	printList(&head3);
+
 	return 0;
 }
 
@@ -78,24 +63,111 @@ Position createPolinom(int coeff, int exp) {
 }
 
 
-void insertStart(Position head, Position K) {
+void insertEnd(Position head, Position K) {
+	while (head->next != NULL)
+		head = head->next;
 	
 	K->next = head->next;
 	head->next = K;
+}
 
+void readFile(char* fileName,Position head1,Position head2) {
+	Position P=NULL;
+	Position Q=NULL;
+	int coeff = 0, exp = 0;
+
+	FILE* File;
+	File = fopen(fileName, "r");
+	if (!File)
+		printf("File failed to open!");
+	char c;
+	while (!feof(File))
+	{
+		c = fgetc(File);
+		if (c != '\n') {
+			fscanf(File, "%d %d", &coeff, &exp);
+			P = createPolinom(coeff, exp);
+			insertEnd(head1, P);
+		}
+		else {
+			while (!feof(File)) {
+				fscanf(File, "%d %d", &coeff, &exp);
+				Q = createPolinom(coeff, exp);
+				insertEnd(head2, Q);
+			}
+		}
+	}
+}
+
+void sumPolinom(Position P, Position Q, Position head3) {
+	Position S = NULL;
+	P = P->next;
+	Q = Q->next;
+	int coeff = 0;
+
+	while (P != NULL || Q != NULL) {
+		if (P == NULL)
+			break;
+		if (Q == NULL)
+			break;
+		if (P->exp == Q->exp) {
+			coeff = P->coeff + Q->coeff;
+			if (coeff != 0) {
+				S = createPolinom(coeff, P->exp);
+				insertEnd(head3, S);
+			}
+			P = P->next;
+			Q = Q->next;
+		}
+		else if (P->exp < Q->exp) {
+			coeff = P->coeff;
+			if (coeff != 0) {
+				S = createPolinom(coeff, P->exp);
+				insertEnd(head3, S);
+			}
+			P = P->next;
+		}
+		else {
+			coeff = Q->coeff;
+			if (coeff != 0) {
+				S = createPolinom(coeff, Q->exp);
+				insertEnd(head3, S);
+			}
+			Q = Q->next;
+		}
+	}
+	if (P == NULL) {
+		while (Q != NULL) {
+			coeff = Q->coeff;
+			if (coeff != 0) {
+				S = createPolinom(coeff, Q->exp);
+				insertEnd(head3, S);
+			}
+			Q = Q->next;
+		}
+	}
+	if (Q == NULL) {
+		while (P != NULL) {
+			coeff = P->coeff;
+			if (coeff != 0) {
+				S = createPolinom(coeff, P->exp);
+				insertEnd(head3, S);
+			}
+			P = P->next;
+		}
+	}
 }
 
 void printList(Position head) {
 	
 	Position K = NULL;
 	
-	printf("\nlist content:\n");
 	if (head->next == NULL)
 		printf("Linked list is empty");
 	
 	K = head->next;
 	while (K != NULL) {
-		printf("\t%d %d\n", K->coeff, K->exp);
+		printf("\t%d\t%d\n", K->coeff, K->exp);
 		K = K->next;
 	}
 
