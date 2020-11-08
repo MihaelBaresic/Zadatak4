d9a2a1e752009c04b43c41af8e42c37d958d96f3
Mihael72 <mihael.baresic@gmail.com>
2020-11-08 01:50:53 +0100

    Added basics

--- Source4.c
+++ Source4.c
@@ -15,41 +15,89 @@ typedef struct _polinom {
 
 
 Position createPolinom(int, int);
+void insertStart(Position, Position);
+void printList(Position);
 
 int main() {
 	FILE* File;
 	Position P = NULL;
-	Position head = NULL;
+	Position Q = NULL;
+	polinom head1;
+	polinom head2;
+	head1.next = NULL;
+	head2.next = NULL;
 	int coeff = 0, exp = 0;
 	char datName[256];
-	printf("Unesi ime datoteke: ");
-	scanf("%s", datName);
+	printf("Insert file name: ");
+	scanf("%s",datName);
 	File = fopen(datName, "r");
 	if (!File)
 		printf("File failed to open!");
 
-	int n;
+	int n=0,i;
 	char* buffer = malloc(1024);
 	while (1) {
 		if (fgets(buffer, 50, File) == '\0')
 			break;
 		n++;
 	}
-	for (int i = 0; i < n; i++) {
-		fscanf(File, "%d %d", &coeff, &exp);
-		P = createPolinom(coeff, exp);
+	rewind(File);
+	char c;
+	while (!feof(File)) 
+	{
+		c = fgetc(File);
+		if (c != '\n') {
+			fscanf(File, "%d %d", &coeff, &exp);
+			P = createPolinom(coeff, exp);
+			insertStart(&head1, P);
+		}else {
+			while (!feof(File)) {
+				fscanf(File, "%d %d", &coeff, &exp);
+				Q = createPolinom(coeff, exp);
+				insertStart(&head2, Q);
+			}
+		}
 	}
+	printList(&head1);
+	printList(&head2);
+	return 0;
 }
 
-Position createStudent(int coeff, int exp) {
-	Position P = NULL;
+Position createPolinom(int coeff, int exp) {
+	Position K = NULL;
 
-	P = (Position)malloc(sizeof(polinom));
-	if (!P)
+	K = (Position)malloc(sizeof(polinom));
+	if (!K)
 		printf("Memory failed to allocate!");
 
-	P->coeff = coeff;
-	P->exp = exp;
+	K->coeff = coeff;
+	K->exp = exp;
+	K->next = NULL;
 
-	return P;
+	return K;
+}
+
+
+void insertStart(Position head, Position K) {
+	
+	K->next = head->next;
+	head->next = K;
+
+}
+
+void printList(Position head) {
+	
+	Position K = NULL;
+	
+	printf("\nlist content:\n");
+	if (head->next == NULL)
+		printf("Linked list is empty");
+	
+	K = head->next;
+	while (K != NULL) {
+		printf("\t%d %d\n", K->coeff, K->exp);
+		K = K->next;
+	}
+
+	puts("\n");
 }
